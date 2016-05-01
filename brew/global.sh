d91ef1ebeae2379f3a077f9346431bbc76450ec6
Jonas Mendes <jonas@nipher.io>
2016-05-01 14:02:57 -0300

    Adding zsh

--- brew/global.sh
+++ brew/global.sh
@@ -17,6 +17,11 @@ else
     ok "OK"
 fi
 
+# zsh
+brew install zsh zsh-completions
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+chsh -s /usr/local/bin/zsh
+
 #vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
